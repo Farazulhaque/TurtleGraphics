ca3371b0a4e4f6f4df9c4f64a4eff1e7ad2e5a95
Faraz <farazattari786@gmail.com>
2021-09-17 23:34:10 +0530

    draw lines of multiple colors

---
+++ multipleColorsLines.py
@@ -0,0 +1,28 @@
+import turtle
+
+# get user input data
+initial_len = int(input("Enter the length of the first line: "))
+angle = int(input("Enter the turning angle: "))
+n = int(input("Enter the number of lines you need: "))
+speed = int(input("Enter the animation speed: "))
+
+pen = turtle.Turtle()
+pen.speed(speed)
+pen.pensize(2)
+
+# start drawing lines
+for i in range(n):
+    if i % 3 == 0:
+        pen.pencolor("red")
+        pen.forward(initial_len*i+1)
+    elif i % 3 == 1:
+        pen.pencolor("green")
+        pen.forward(initial_len*i+2)
+    elif i % 3 == 2:
+        pen.pencolor("blue")
+        pen.forward(initial_len*i+3)
+    pen.left(angle)
+
+turtle.done()
+
+
