cfbb964adebb1537f764b4834a36b93a8d8fdbfd
Farazulhaque <farazattari786@gmail.com>
2021-10-02 09:47:41 +0530

    polygons function

---
+++ polygons.py
@@ -0,0 +1,22 @@
+from turtle import *
+import turtle
+
+Kat = turtle.Turtle()
+Kat.speed(0)
+
+def spinPolygon(tortoise, nSides, sidelength, angle, nCopies):
+    for i in range(nCopies):
+        for j in range(nSides):
+            tortoise.forward(sidelength)
+            tortoise.left(90)
+        tortoise.left(angle)
+    
+
+# spinPolygon(Kat, 4, 50, 30, 5)
+
+# spinPolygon(Kat, 4, 50, -30, 3)
+
+spinPolygon(Kat, 4, 50, 10, 100)
+
+turtle.done()
+
